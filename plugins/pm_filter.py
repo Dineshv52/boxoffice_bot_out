d6db6a4f5c5d27b36cf79e2553682cb918fd07e6
Dinesh V <dinesh.v@zohocorp.com>
2024-01-17 15:33:56 +0530

    owner updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1268,7 +1268,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/DwayneJohnsonl")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/bugbountyhunt")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
